cd5fe20f445a60f02868af4cc274f65c01e0f077
NafisFuadTanvir <nafisf026@gmail.com>
2025-02-15 09:05:34 +0600

    creating the serializer for the custom user, and doing the authentication logic

---
+++ user/serializers.py
@@ -0,0 +1,54 @@
+from .models import CustomUser
+from rest_framework import serializers
+from django.core.mail import EmailMultiAlternatives
+from django.template.loader import render_to_string
+from django.utils.crypto import get_random_string
+from django.urls import reverse
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model= CustomUser
+        fields= ['id','username','email','password','bio','image']
+        #hiding the password
+        extra_kwargs={"password": {"write only": True}}
+       
+        #modifying the inbuild function create
+        def create(self,validated_data):
+            user= CustomUser(**validated_data)
+            user.set_password(validated_data['password'])
+            user.verification_token=get_random_string(length=32)
+            user.save()
+            self.send_email(user=user)
+            return user
+        
+        def send_email(self,user):
+            
+            #generating verification link
+            verification_link=self.context['request'].build.absolute_uri(
+                reverse(
+                    viewname='verify email',
+                    kwargs={"token":user.verification_token}
+                    
+                )
+            )
+            #render the email template
+            subject="verify your email"
+            html_content= render_to_string('emails/verification_email.html',{
+                "user":user.username,
+                "verification_link":verification_link
+            })
+            
+            #create an email message
+            email= EmailMultiAlternatives(
+                subject,
+                "this is a text version of the email",
+                "from@example.com",
+                [user.email]
+                )
+            email.attach_alternative(html_content, "text/html")
+            email.send(fail_silently=False)
+            
+            return True
+            
+            
+        
--- user/views.py
+++ user/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework import generics,status
+from .serializers import UserSerializer
+from .models import CustomUser
+from rest_framework.response import Response
 
-# Create your views here.
+
+#the createapiview only deals with the post request
+class UserSignup(generics.CreateAPIView):
+    serializer_class= UserSerializer
+    
+    
