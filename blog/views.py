4c2af9ad305738c83e8615bb31adb8076a6e6719
NafisFuadTanvir <nafisf026@gmail.com>
2025-02-19 14:30:28 +0600

    implement create,delete,update,list blog views with authentication also blog details and personal blog list is added

--- blog/views.py
+++ blog/views.py
@@ -6,3 +6,65 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.pagination import PageNumberPagination
 from django.http import Http404
+
+# creating pagination class
+class Blogpagination(PageNumberPagination):
+    page_size=5
+    page_size_query_param='page_size'
+    max_page_size=10
+
+#blog listing view    
+class Bloglistview(generics.ListAPIView):
+    queryset= Blog.objects.all().order_by('-created_at') 
+    serializer_class= Blogserializer
+    pagination_class=Blogpagination  
+ 
+#personal blog listing view
+class Myblogs(generics.ListAPIView):
+    serializer_class=Blogserializer
+    permission_classes=[IsAuthenticated]
+    pagination_class=Blogpagination
+    
+    def get_queryset(self):
+        return Blog.objects.filter(author=self.request.user)
+       
+
+#single blog detail_view
+class Blogdetailsview(generics.RetrieveAPIView):
+    queryset=Blog.objects.all()
+    serializer_class=Blogserializer
+    lookup_field='id'
+
+#blog create view
+class Blogcreateview(generics.CreateAPIView):
+    serializer_class= BlogCreateUpdateserializer
+    permission_classes=[IsAuthenticated] 
+    
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+#blog update view
+class Blogupdateview(generics.UpdateAPIView):
+    queryset=Blog.objects.all()
+    serializer_class= BlogCreateUpdateserializer
+    permission_classes= [IsAuthenticated]
+    lookup_field='id'
+    
+    def get_object(self):
+        blog=super().get_object()
+        if blog.author != self.request.user:
+            raise Http404
+        return blog  
+
+#blog delete views
+class Blogdeleteview(generics.DestroyAPIView):
+    queryset=Blog.objects.all()
+    permission_classes= [IsAuthenticated]
+    lookup_field='id'
+    
+    def get_object(self):
+        blog=super().get_object()
+        if blog.author != self.request.user:
+            raise Http404
+        return blog  
+                   
